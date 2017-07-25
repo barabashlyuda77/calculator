ed14f3380d66a92aad9fd6a5343b781a1f58a11f
Lyuda Barabash <barabashlyuda@gmail.com>
2017-07-25 17:16:55 +0300

    Create repository

---
+++ src/layouts/PageLayout/PageLayout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './PageLayout.scss'
+
+export const PageLayout = () => (
+    <div className='container text-center'>
+        <h1>Calculator</h1>
+    </div>
+)
+
+export default PageLayout
